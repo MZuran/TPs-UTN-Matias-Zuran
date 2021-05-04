385fc63cd33f0a8071530815ecfeea1292869976
MZuran <83543640+MZuran@users.noreply.github.com>
2021-05-04 19:26:12 -0300

    Add files via upload

---
+++ TP1 Matias Zuran/TP1-EJ1-MZuran/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float v1 = 0;
+    cout << "Ingrese el el valor de km/h" << endl;
+    cin >> v1;
+    cout << v1 << " km/h es " << v1/3.6 << " m/s";
+}
